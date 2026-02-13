c2809278a7703376b2111c1c4fb7205d5678daa3
Rahul Kumar <rahul Kumar>
2026-02-13 22:12:06 +0530

    libuv concept is clear

--- class_3_Libuv_nodejs/index.js
+++ class_3_Libuv_nodejs/index.js
@@ -1,3 +1,21 @@
+// setTimeout(()=>{
+//     consoloe.log('Hello world');
+// },3000)
+
+const fs =require('fs');
+fs.readFile('./data.json','utf-8',(err,res)=>{
+    console.log(res);
+})
+
+let a= 10;
+let b=20;
+
+console.log(b);
+
 setTimeout(()=>{
-    consoloe.log('Hello world');
-},3000)
+    console.log(a);
+},2000)
+
+console.log('Hello world');
+console.log('second line');
+
